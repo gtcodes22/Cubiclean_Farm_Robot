8b1f9b899bf92fc4cb6fe69e04f69f005f597027
Raving-Rabbit <93443678+Raving-Rabbit@users.noreply.github.com>
2025-12-21 11:43:02 +0000

    Delete turtlebot3_ws/build directory
    Hopefully I can actually push my changes now!

--- turtlebot3_ws/build/turtlebot3_drive/ament_cmake_symlink_install_targets_0_.cmake
+++
@@ -1,1 +0,0 @@
-ament_cmake_symlink_install_targets("TARGET_FILES" "/home/gideon/turtlebot3_ws/build/turtlebot3_drive/turtlebot3_drive" "TARGETS" "turtlebot3_drive" "DESTINATION" "lib/turtlebot3_drive")
--- turtlebot3_ws/build/turtlebot3_simulations/colcon_command_prefix_build.sh
+++
@@ -1,4 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/gideon/turtlebot3_ws/install/turtlebot3_gazebo/share/turtlebot3_gazebo/package.sh"
-. "/home/gideon/turtlebot3_ws/install/turtlebot3_msgs/share/turtlebot3_msgs/package.sh"
-. "/home/gideon/turtlebot3_ws/install/turtlebot3_fake_node/share/turtlebot3_fake_node/package.sh"
