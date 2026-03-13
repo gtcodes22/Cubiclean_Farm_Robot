f44c7dea0cd40dbd9b83d5546b66f36652e75b37
Raving-Rabbit <93443678+Raving-Rabbit@users.noreply.github.com>
2026-03-13 23:25:43 +0000

    Added two files to process bed data
    Started work on files which process data for sending to the app

---
+++ server/BedData.py
@@ -0,0 +1,7 @@
+class BedData():
+    def __init__(self, bednum, readDate, readTime):
+        self.bednum = bednum
+        self.readDate = date
+        self.readTime = time
+        self.points = [None]*6
+        
---
+++ server/process_bed_data.py
@@ -0,0 +1,13 @@
+from bed_webview.Extract_Files import Extract_filename
+from server.BedData import BedData
+
+def process_bed_data(raw_data_folder='bed_data',proc_data_folder='bed_app_data'):
+    # starting at bed 0 point 1
+    
+    # for each file...
+    Extract_filename(file_name)
+    
+    # check to see if this bed has already been processed
+    
+    # get mean for each point
+    # get mean for whole bed
