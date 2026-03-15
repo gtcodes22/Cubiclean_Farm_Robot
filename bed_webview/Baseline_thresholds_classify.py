231d361b51beb7bbdeb17000f059fbd4af4ae789
Raving-Rabbit <93443678+Raving-Rabbit@users.noreply.github.com>
2026-03-15 04:38:16 +0000

    Create Baseline_thresholds_classify.py

---
+++ bed_webview/Baseline_thresholds_classify.py
@@ -0,0 +1,8 @@
+Baseline_thresholds_classify = {
+	"Temp_Air": {"Min": -10.0, "Max": 45.0},
+	"Humi_Air": {"Min": 0.0, "Max": 100.0},
+	"CO2_ppm": {"Min": 300.0, "Max": 1500.0},
+	"NH3_ppm": {"Min": 0.0, "Max": 15.0},
+	"H2S_ppm": {"Min": 0.0, "Max": 5.0},
+	"CH4_Vout": {"Min": 0.0, "Max": 1.8},
+}
