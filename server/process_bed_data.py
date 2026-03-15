5bdd43ebf0dacd9a86e9c8fad96488b8dc11a576
Raving-Rabbit <93443678+Raving-Rabbit@users.noreply.github.com>
2026-03-15 04:08:18 +0000

    completed process_bed_data
    'process_bed_data' gets mean for each reading for each point and writes it to a file in bed_app_data (by default)

--- server/process_bed_data.py
+++ server/process_bed_data.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 from bed_webview.Extract_Files import Extract_filename
 from server.BedData import BedData
@@ -45,25 +46,78 @@ def get_latest_bed_data(rawDataFolder='bed_data',procDataFolder='bed_app_data'):
     # for each bed print all the points (should be the latest)
     for bed in beds:
         if bed:
-            procFilename = f'BED{bed.bednum}_{bed.timestamp}'
+            procFilename = f'BED{bed.bednum}_{bed.timestamp}.csv'
             
-            # process the bed data
+            # check to see if this bed has already been processed and
+            # process the bed data if not
             if not procFilename in bedProcFiles:
-                print(f'processing BED{bed.bednum} (bed.timestamp)')
-                process_bed_data(bed)
+                print(f'processing BED{bed.bednum} ({bed.timestamp})')
+                process_bed_data(bed, rawDataFolder, procDataFolder)
                 
             for i in range(6):
                 print(f'{bed.bednum}:{i + 1} - {bed.points[i]}')
     
-    # check to see if this bed has already been processed
     
     
     # get mean for each point
     # get mean for whole bed
 
-def process_bed_data(bed : BedData):
-    procFilename = f'BED{bed.bednum}_{bed.timestamp}'
-    return
+def process_bed_data(bed : BedData, rawDataFolder='bed_data', procDataFolder='bed_app_data'):
+    procFilename = f'{procDataFolder}/BED{bed.bednum}_{bed.timestamp}.csv'
+    procFile = open(procFilename, 'w', newline='')
+    procFileWritter = csv.writer(procFile, delimiter=',')
+    
+    procFileWritter.writerow(['Point', 'Temp_Air', 'Humi_Air', 'CO2_ppm', 'NH3_ppm', 'H2S_ppm', 'CH4_Vout'])
+    
+    for i in range(6):
+        with open(f'{rawDataFolder}/{bed.points[i]}', newline='') as pointFile:
+            pointRows = csv.reader(pointFile)
+            
+            # skip headings row
+            pointFile.readline()
+            
+            readings = 0
+            missingReadings = [0] * 6
+            
+            Temp_Air = 0.0
+            Humi_Air = 0.0
+            CO2_ppm = 0.0
+            NH3_ppm = 0.0
+            H2S_ppm = 0.0
+            CH4_Vout = 0.0
+            
+            # sum values together
+            for row in pointRows:
+                try:
+                    # check all readings are there
+                    for item in row:
+                        float(item)
+                        
+                    Temp_Air += float(row[0])
+                    Humi_Air += float(row[1])
+                    CO2_ppm  += float(row[2])
+                    NH3_ppm  += float(row[3])
+                    H2S_ppm  += float(row[4])
+                    CH4_Vout += float(row[5])
+                    readings += 1
+                except ValueError:
+                    # skip any rows that has a missing reading
+                    print('process_bed_data: skipping row')
+                    continue
+            
+            # get mean of values
+            Temp_Air /= readings
+            Humi_Air /= readings 
+            CO2_ppm  /= readings
+            NH3_ppm  /= readings
+            H2S_ppm  /= readings
+            CH4_Vout /= readings
+            
+            # write output to processed data file
+            procFileWritter.writerow([i, Temp_Air, Humi_Air, CO2_ppm, NH3_ppm, H2S_ppm, CH4_Vout])
+    
+    procFile.flush()
+    procFile.close()
     
 if __name__ == "__main__":
     process_bed_data()
